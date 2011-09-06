c60c71ea7efed6a1b29d35befee3296e2ed4a6c5
Pablo Diez <pablodiez@zaubersoftware.com>
2011-09-06 12:27:11 -0300

    Fix in delete operation.

--- src/main/java/org/mule/modules/jive/api/impl/CustomDeleteOperation.java
+++ src/main/java/org/mule/modules/jive/api/impl/CustomDeleteOperation.java
@@ -12,6 +12,7 @@ package org.mule.modules.jive.api.impl;
 
 import org.mule.modules.jive.api.EntityType;
 import org.mule.modules.jive.api.PayloadOperation;
+import org.mule.modules.jive.api.ReferenceOperation;
 import org.mule.modules.jive.api.xml.XmlMapper;
 
 import com.sun.jersey.api.client.WebResource;
@@ -19,7 +20,7 @@ import com.sun.jersey.api.client.WebResource;
 import java.io.StringReader;
 import java.util.Map;
 
-public class CustomDeleteOperation implements PayloadOperation
+public class CustomDeleteOperation implements ReferenceOperation
 {
     
     private final String uriException;
@@ -30,13 +31,10 @@ public class CustomDeleteOperation implements PayloadOperation
     }
 
     /* (non-Javadoc)
-     * @see org.mule.modules.jive.api.PayloadOperation#execute(com.sun.jersey.api.client.WebResource, org.mule.modules.jive.api.xml.XmlMapper, org.mule.modules.jive.api.EntityType, java.util.Map)
+     * @see org.mule.modules.jive.api.ReferenceOperation#execute(com.sun.jersey.api.client.WebResource, org.mule.modules.jive.api.xml.XmlMapper, org.mule.modules.jive.api.EntityType, java.lang.String)
      */
     @Override
-    public Map<String, Object> execute(WebResource resource,
-                                       XmlMapper mapper,
-                                       EntityType type,
-                                       Map<String, Object> entityData)
+    public Map<String, Object> execute(WebResource resource, XmlMapper mapper, EntityType type, String id)
     {
         final String response = resource.path(this.uriException).delete(String.class);
         return mapper.xml2map(new StringReader(response));
